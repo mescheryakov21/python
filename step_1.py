af0d7ff99499d593fa4de950eedcbf23c6d36f40
mescheryakov21 <volody1987@mail.ru>
2021-07-25 00:52:20 +0300

    praktic lesson 1

--- step_1.py
+++ step_1.py
@@ -1,5 +1,8 @@
-import tkinter
-
-import PIL
-
-print('hello world')
+a = 3
+b = 4 / a
+c = "Love is..."
+print(a + b)
+name = input('Как Ваше Имя: ')
+age = input("Сколько Вам лет?: ")
+d = a + b + int(age)
+print("Пользователя зовут: " + str(name) + " Возраст равен: " + age)
